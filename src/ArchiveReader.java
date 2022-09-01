ca00568251fdfc9a05a844d64dd960026f23a165
Thiago Figueiredo <thiagosfig@gmail.com>
2022-09-01 19:08:37 -0300

    added some shit such as lists and delimiters

--- src/ArchiveReader.java
+++ src/ArchiveReader.java
@@ -1,19 +1,20 @@
-import lombok.*;
-
 import java.io.*;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@Data
 public class ArchiveReader {
 
+    private List<String> decl = Arrays.asList("class", "extends", "public", "static", "void", "main", "length", "this", "new");
+    private List<String> fluxo = Arrays.asList("if", "else", "while", "return", "System.out.println");
+    private List<String> tipo = Arrays.asList("boolean", "int", "true", "false", "String");
     private String name;
     private FileInputStream file;
     private Pattern numberRegex = Pattern.compile("(^[0-9]+$)", Pattern.CASE_INSENSITIVE);
     private Pattern idRegex = Pattern.compile("^([a-z])([0-9]|[a-z])*$", Pattern.CASE_INSENSITIVE);
-    private Pattern opRegex = Pattern.compile("[-+*/=]", Pattern.CASE_INSENSITIVE);
-
+    private Pattern opRegex = Pattern.compile("[-+*/=&<>!.,]", Pattern.CASE_INSENSITIVE);
+    private Pattern delimRegex = Pattern.compile("[(){};]", Pattern.CASE_INSENSITIVE);
     public ArchiveReader(String name) {
         this.name = name;
 
@@ -156,21 +157,30 @@ public class ArchiveReader {
         Matcher numberMatcher = numberRegex.matcher(str);
         Matcher idMatcher = idRegex.matcher(str);
         Matcher opMatcher = opRegex.matcher(str);
+        Matcher delimMatcher = delimRegex.matcher(str);
 
         boolean foundOp = false;
+        boolean foundDelim = false;
 
         char operator = '$';
+        char delim = '$';
 
         try {
             while(((myData = this.file.read()) != -1)) {
                 opMatcher = opRegex.matcher("" + (char)myData);
+                delimMatcher = delimRegex.matcher("" + (char)myData);
 
                 if(opMatcher.find()) {
                     foundOp = true;
-                    operator = (char)myData;
+                    operator = (char) myData;
                 }
 
-                if((char)myData != ' ' && (char)myData != '\n' && (char)myData != ';' && !foundOp) {
+                if(delimMatcher.find()) {
+                    foundDelim = true;
+                    delim = (char) myData;
+                }
+
+                if((char)myData != ' ' && (char)myData != '\n' && (char)myData != ';' && !foundOp && !foundDelim) {
                     str = str + (char) myData;
                 } else {
 
@@ -180,7 +190,15 @@ public class ArchiveReader {
                     if(numberMatcher.find()) {
                         System.out.println("<number, " + str + ">");
                     } else if (idMatcher.find()){
-                        System.out.println("<id, " + str + ">");
+                        if (decl.contains(str)){
+                            System.out.println("<decl, " + str + ">");
+                        } else if (fluxo.contains(str)) {
+                            System.out.println("<fluxo, " + str + ">");
+                        } else if (tipo.contains(str)) {
+                            System.out.println("<tipo, " + str + ">");
+                        } else {
+                            System.out.println("<id, " + str + ">");
+                        }
                     }
 
                     if(operator != '$'){
@@ -188,8 +206,14 @@ public class ArchiveReader {
                         operator = '$';
                     }
 
+                    if(delim != '$'){
+                        System.out.println("<delim, " + delim + ">");
+                        delim = '$';
+                    }
+
                     str = "";
                     foundOp = false;
+                    foundDelim = false;
                 }
             }
             Matcher matcher = idRegex.matcher(str);
